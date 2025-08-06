afd2bb15c9496629dd2b39590928e95c64896fee
Laudier2 <laudiersantanamei@gmail.com>
2025-08-06 01:00:15 -0300

    Remove Stripe test key

--- frontend/src/components/UrlListlist.tsx
+++ frontend/src/components/UrlListlist.tsx
@@ -68,7 +68,7 @@ export const UrlListlist: React.FC<Props> = ({ urls }) => {
 
       <ul style={{ listStyleType: 'none', padding: 0 }}>
         {urls.map((url) => {
-          const shortUrl = `http://localhost:4000/${url.slug}`;
+          const shortUrl = `https://urlcurt.site/${url.slug}`;
           const isExpanded = expandedUrlId === url.id;
           const isLoading = loadingTraffic === url.id;
           const history = trafficData[url.id];
